4a00edc758e1329971c353849b086dc813fecb32
unknown <nikhilpillare98@gmail.com>
2022-11-30 20:11:15 +0530

    check

---
+++ palindrome.js
@@ -0,0 +1,19 @@
+let n = 6;
+let str = nrupul;
+
+let sum = "";
+for(let i=0; i<str.length; i++){
+    sum += str[i]+"";
+}
+
+let a = "";
+for(let i = str.length-1; i>=0; i--){
+    a+= str[i]+"";
+}
+
+if(sum ==a){
+    console.log("No")
+}
+else{
+    console.log("Yes")
+}
